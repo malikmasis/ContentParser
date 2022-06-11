8bd57bd67e60448ec8131f45c98bf91b082a9d7a
malik masis <malik.masis@gmail.com>
2022-06-12 00:15:07 +0300

    fixed the structure of the project

--- ContentParser/ContentParser.cs
+++ ContentParser/ContentParser.cs
@@ -4,88 +4,76 @@ namespace ContentParser;
 
 public class ContentParser
 {
-    public async Task ParseAsync()
+    private readonly List<string> _options;
+    public ContentParser()
     {
-        string content0 = @"**ABP Framework** is completely open source and developed in a community-driven manner.
-
-                    [Widget Type=""Poll"" PollName=""poll-name""]
-                    Thanks _for_ *your * feedback.
-                    [Widget Type=""Poll"" PollName=""poll-name1""]";
-
-        string content1 = @"**ABP Framework** is completely open source and developed in a community-driven manner.
-                    Thanks _for_ *your * feedback.
-                    [Widget Type=""Poll"" PollName=""poll-name""]";
-
-        string content2 = @"[Widget Type=""Poll"" PollName=""poll-name""] gg [Widget Type=""Poll"" PollName=""poll-name1""]**ABP Framework** is completely open source and developed in a community-driven manner.
-                    Thanks _for_ *your * feedback.
-                    Thanks _for_ *your * feedback.";
-
-        string content3 = @"Thanks _for_ *your * feedback.
-                    Thanks _for_ *your * feedback.";
-
-        string content4 = @"[Widget Type=""Poll"" PollName=""poll-name""]";
-
-        var contents = new List<string>() { content0, content1, content2, content3, content4 };
-
-        foreach (var content in contents)
+        _options = new()
         {
-            var polls = Regex.Matches(content, @"\[.*?\]").Select(p => p.Value).ToList();
+            "Poll"
+        };
+    }
 
-            string delimeter = "----";
-            var replacedText = Regex.Replace(content, @"\[.*?\]", delimeter);
+    public Task<List<string>> ParseAsync(string content)
+    {
+        string delimeter = "----";
+        var replacedText = Regex.Replace(content, @"\[.*?\]", delimeter);
 
-            var parsedList = new List<string>();
-            if (!replacedText.Contains(delimeter))
+        var parsedList = new List<string>();
+        if (!replacedText.Contains(delimeter))
+        {
+            parsedList.Add(replacedText);
+        }
+
+        while (replacedText.Contains(delimeter))
+        {
+            //For parsing delimeter
+            var index = replacedText.IndexOf(delimeter);
+            if (index != 0)
+            {
+                parsedList.Add(replacedText.Substring(0, index));
+                replacedText = replacedText.Substring(index, replacedText.Length - index);
+                index = 0;
+            }
+
+            parsedList.Add(replacedText.Substring(index, delimeter.Length));
+            replacedText = replacedText.Substring(delimeter.Length, replacedText.Length - delimeter.Length);
+
+            //for parsing the other side
+            index = replacedText.IndexOf(delimeter);
+            if (index != -1)
+            {
+                parsedList.Add(replacedText.Substring(0, index));
+                replacedText = replacedText.Substring(index, replacedText.Length - index);
+
+            }
+            else
             {
                 parsedList.Add(replacedText);
             }
-
-            while (replacedText.Contains(delimeter))
-            {
-                //For parsing delimeter
-                var index = replacedText.IndexOf(delimeter);
-                if (index != 0)
-                {
-                    parsedList.Add(replacedText.Substring(0, index));
-                    replacedText = replacedText.Substring(index, replacedText.Length - index);
-                    index = 0;
-                }
-
-                parsedList.Add(replacedText.Substring(index, delimeter.Length));
-                replacedText = replacedText.Substring(delimeter.Length, replacedText.Length - delimeter.Length);
-
-                //for parsing the other side
-                index = replacedText.IndexOf(delimeter);
-                if (index != -1)
-                {
-                    parsedList.Add(replacedText.Substring(0, index));
-                    replacedText = replacedText.Substring(index, replacedText.Length - index);
-
-                }
-                else
-                {
-                    parsedList.Add(replacedText);
-                }
-            }
-
-
-
-            for (int b = 0, k = 0; b < parsedList.Count; b++)
-            {
-                if (parsedList[b] == delimeter)
-                {
-                    Console.WriteLine(polls[k++]);
-                }
-                else
-                {
-                    Console.WriteLine(parsedList[b]);
-                }
-            }
-
-            Console.WriteLine("--------");
         }
 
-        await Task.CompletedTask;
+        var pollNames = Regex.Matches(content, @"(?<=PollName="")(.*?)(?="")").Select(p => p.Value).ToList();
+        var polls = Regex.Matches(content, @"(?<=Widget Type="")(.*?)(?="")").Select(p => p.Value).ToList();
+
+        List<string> contentFragments = new();
+        for (int i = 0, k = 0; i < parsedList.Count; i++)
+        {
+            if (parsedList[i] == delimeter)
+            {
+                var name = _options.FirstOrDefault(p => p == polls[k]);
+                if (name is not null)
+                {
+                    contentFragments.Add($"{name}-{pollNames[k]}");
+                }
+                k++;
+            }
+            else
+            {
+                contentFragments.Add(parsedList[i]);
+            }
+        }
+
+        return Task.FromResult(contentFragments);
     }
 }
 
--- ContentParser/Program.cs
+++ ContentParser/Program.cs
@@ -9,12 +9,40 @@ public class Program
     {
 
     }
-    static void Main(string[] args)
+
+    static Task Main(string[] args)
     {
+        string content0 = @"**ABP Framework** is completely open source and developed in a community-driven manner.
+
+                    [Widget Type=""Poll"" PollName=""poll-name""]
+                    Thanks _for_ *your * feedback.
+                    [Widget Type=""Poll"" PollName=""poll-name1""]";
+
+        string content1 = @"**ABP Framework** is completely open source and developed in a community-driven manner.
+                    Thanks _for_ *your * feedback.
+                    [Widget Type=""Poll"" PollName=""poll-name""]";
+
+        string content2 = @"[Widget Type=""Poll"" PollName=""poll-name""] gg [Widget Type=""Poll"" PollName=""poll-name1""]**ABP Framework** is completely open source and developed in a community-driven manner.
+                    Thanks _for_ *your * feedback.
+                    Thanks _for_ *your * feedback.";
+
+        string content3 = @"Thanks _for_ *your * feedback.
+                    Thanks _for_ *your * feedback.";
+
+        string content4 = @"[Widget Type=""Poll"" PollName=""poll-name""]";
+
         try
         {
-            ContentParser parser = new();
-            Task.FromResult(parser.ParseAsync());
+            var contents = new List<string>() { content0, content1, content2, content3, content4 };
+
+            foreach (var content in contents)
+            {
+                ContentParser parser = new();
+                var contentFragments = parser.ParseAsync(content).Result;
+                Console.WriteLine(string.Join(" ", contentFragments));
+                Console.WriteLine();
+            }
+            return Task.CompletedTask;
         }
         catch (Exception ex)
         {
